53e5bb220dfd6d2122040caf6cb9d4eff4831aa2
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-11-22 16:20:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -35,6 +35,28 @@ var push_to_firebase = function(data){
       }
 
       var contact_submit = function(){
+              
+              
+                      // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function() {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
          
          
          var apaterno = document.getElementById("apaterno");
@@ -76,26 +98,6 @@ var push_to_firebase = function(data){
            "comentario": comentario.value
         }
         
-        // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function() {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
         push_to_firebase(data);
 };
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
