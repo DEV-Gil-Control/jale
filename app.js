5190beae9a766c4ace951994b4b71403a280ed66
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-11-22 19:42:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,21 +7,7 @@ var push_to_firebase = function(data){
 
         db.collection("messages").add({
            
-            apaterno: data["apaterno"],
-            amaterno: data["amaterno"],
-           nacimiento: data["nacimiento"], 
-           telefono: data["telefono"],
-           email: data["email"],
-           estudios: data["estudios"],
-              domicilio: data["domicilio"],
-              colonia: data["colonia"],
-              estado: data["estado"],
-              municipio: data["municipio"],
-              codigo: data["codigo"],
-              horario: data["horario"],
-                sueldomes: data["sueldomes"],
-                comentario: data["comentario"],
-
+            nombres: data["nombres"],
             timestamp: Date.now()
         })
         .then(function(docRef) {
@@ -35,44 +21,12 @@ var push_to_firebase = function(data){
 
       var contact_submit = function(){
            
-           
-         var apaterno = document.getElementById("apaterno");
-         var amaterno = document.getElementById("amaterno");
-         var nacimiento = document.getElementById("nacimiento");
-         var telefono = document.getElementById("telefono");
-         var email = document.getElementById("email");
-         var estudios = document.getElementById("estudios");
-        
-          var domicilio = document.getElementById("domicilio");
-          var colonia = document.getElementById("colonia");
-          var estado = document.getElementById("estado");
-          var municipio = document.getElementById("municipio");
-          var codigo = document.getElementById("codigo");
-         var horario = document.getElementById("horario");
-
-         var sueldomes = document.getElementById("sueldomes");
-         var comentario = document.getElementById("comentario");
         var nombres = document.getElementById("nombres");
 
         var data = {
          
-          "apaterno": apaterno.value,
-          "amaterno": amaterno.value,
-           "nacimiento": nacimiento.value,
-          "telefono": telefono.value,
-            "email": email.value,
-            "estudios": estudios.value,
-            
-            "domicilio": domicilio.value,
-            "colonia": colonia.value,
-            "estado": estado.value,
-            "municipio": municipio.value,
-            "codigo": codigo.value,
-            "horario": horario.value,
-            
-           "sueldomes": sueldomes.value,
-          "nombres": nombres.value,
-           "comentario": comentario.value
+
+          "nombres": nombres.value
         }
         
 
