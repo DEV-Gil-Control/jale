d5a10a882aa8851556eb745f61eeda0d1290f36a
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-11-22 19:14:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,59 @@
-/**
- * formChecker check Required field on form and add red start with span tag
- * before this field or after. You can change all parameters.
- * @author Kevin FERRANDON
- * @version 1.0.0
- * @type Object
- */
-class formChecker {
-    constructor() {
-        this.setRequiered();
-    }
-/**
- * Set text, html tag and color for html tag with required attribut on form
- * @param {string} text
- * @param {string} color
- * @param {string} tag
- * @returns 
- */
-    setRequiered(text='*', color = '#ff0000;', tag='span') {
-        this.sheet = document.createElement('style');
-        this.sheet.innerHTML = ".required{ color: " + color + "}";
-        document.body.appendChild(this.sheet);
-        this.span = document.createElement(tag);
-        this.span.textContent = text;
-        this.span.setAttribute('class', 'required');
-    }
-    /**
-     * Add node before or after input or select field 
-     * @param {string} when
-     * @returns 
-     */
-    addRequired(when='before') {
-        var formsCollection = document.getElementsByTagName("form");
-        for (var j = 0; j < formsCollection.length; j++)
-        {
-            this.elts = document.forms[j].querySelectorAll("[required]");
-            for (var i = 0; i < this.elts.length; i++) {
-                var span = this.span.cloneNode(true);
-                if(when==='before'){
-                    this.elts[i].before(span);
-                }else{
-                    this.elts[i].after(span);
-                }
-                
-            }
+/*Store regex expressions here*/
+const validations = {
+    username: /^[A-Za-z]{8,50}$/,
+    password: /^(?=(.*[^A-Za-z0-9]){3})(?=(.*[A-Z]){3})(?=(.*\d){3}).+/,
+    email: /^[A-Z0-9a-z._%+-]+\@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
+    pancard: /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/,
+    passport: /^[A-Z][1-9]\d\d{4}[1-9]$/
+}
 
+const errorStatuses = {
+    username: "Please enter a name with length greater than 8 characters but less than 50.",
+    password: "Enter a password atleast having three of each : uppercase letters, numerical digits and special characters.",
+    email: "Enter a valid E-mail Id",
+    pancard: "Please enter valid pancard number, Ex. ABCDE1213D , example shows the format not a valid pancard number.",
+    passport: "Please enter a valid passport number. Ex.R2210321"
+}
+
+/*
+ optimize input validation to delay the validation
+ to avoid stray keypresses.
+*/
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function executedFunc() {
+        let context = this;
+        let args = arguments;
+        let later = function () {
+            timeout = null;
+            if (!immediate) {
+                func.apply(context, args);
+            }
+        }
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) {
+            func.apply(context, args);
         }
     }
 }
 
-/**
- * Simple example to use formChecker library 
- * You can uncomment code to check feature.
- * @returns 
- */
-window.onload = function () {
-    var checker = new formChecker();
- //   checker.setRequiered(' * (required)', '#ffcccc', 'p');
-    checker.addRequired();
- //   checker.addRequired('after');
-    
-    
-    
-}
+const delayedValidate = debounce(function validateInput(event) {
+    const messageBox = event.target.nextElementSibling.children[0];
+    validator = validations[event.target.name];
+    let validationStatus = null;
+    if (validator) {
+        validationStatus = validator.test(event.target.value);
+    }
+    if (validationStatus === false) {
+        messageBox.innerText = errorStatuses[event.target.name];
+    } else {
+        messageBox.innerText = "";
+    }
+}, 150);
+
+window.addEventListener("input", delayedValidate);
 
 //- - - - - - - - -  Push Data - - - - - - - - - - - -//
 
