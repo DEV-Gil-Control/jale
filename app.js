c335456d94d231836770bef96e1de330e59a11ee
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-11-22 22:49:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
-function validateName(x) {
-    // Validation rule
-    var re = /[A-Za-z -']$/;
-    // Check input
-    if (re.test(document.getElementById(x).value)) {
-    // Style green
-    document.getElementById(x).style.background ='#ccffcc';
-    // Hide error prompt
-    document.getElementById(x + 'Error').style.display = "none";
-    return true;
-    }else{
-    // Style red
-    
-    // Show error prompt
-    document.getElementById(x + 'Error').style.display = "block";
+function checkForm() {
+  hideAllErrors();
+  var formIsValid =
+    showErrorAndFocusIf('FieldData0', isEmpty, 'nameError')
+    && showErrorAndFocusIf('FieldData2', isEmpty, 'emailError')
+    && showErrorAndFocusIf('FieldData2', isAnInvalidEmail, 'emailError2')
+    && showErrorAndFocusIf('FieldData3', isEmpty, 'categoryError')
+    && showErrorAndFocusIf('FieldData1', isEmpty, 'questionError');
+
+  /* For debugging, lets prevent the form from submitting. */
+  if (formIsValid) {
+    alert("Valid form!");
     return false;
+  }
 
-    
-    // Set error catcher
-    var error = 0;
-    // Check name
-    if (!validateName('name')) {
-    document.getElementById('nameError').style.display = "block";
-    error++;
+  return formIsValid;
+}
 
-   
-    // Don't submit form if there are errors
-    if (error > 0) {
+function showErrorAndFocusIf(fieldId, predicate, errorId) {
+  var field = document.getElementById(fieldId);
+  if (predicate(field)) {
+    document.getElementById(errorId).style.display = 'inline';
+    if (field.select) {
+      field.select();
+    }
+    field.focus();
     return false;
+  }
 
+  return true;
+}
+
+function isEmpty(field) {
+  return field.value == '';
+}
+
+function isAnInvalidEmail(field) {
+  var email = field.value;
+
+  var ok = "1234567890qwertyuiop[]asdfghjklzxcvbnm.@-_QWERTYUIOPASDFGHJKLZXCVBNM";
+
+  for(i = 0; i < email.length; i++){
+    if(ok.indexOf(email.charAt(i)) < 0) {
+      return true;
+    }
+  }
+
+  re = /(@.*@)|(\.\.)|(^\.)|(^@)|(@$)|(\.$)|(@\.)/;
+  re_two = /^.+\@(\[?)[a-zA-Z0-9\-\.]+\.([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  return re.test(email) || !re_two.test(email);
+}
+
+
+
+function hideAllErrors() {
+  document.getElementById("nameError").style.display = "none"
+  document.getElementById("emailError").style.display = "none"
+  document.getElementById("emailError2").style.display = "none"
+  document.getElementById("categoryError").style.display = "none"
+  document.getElementById("questionError").style.display = "none"
+}
 
 //- - - - - - - - -  Push Data - - - - - - - - - - - -//
 
