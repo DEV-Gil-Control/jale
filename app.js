7be7d50b90e5db2a3ec277838a7f0b5cd563b4ac
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-11-22 21:39:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,27 @@ let submitButton = document.getElementById('submit')
 submitButton.addEventListener("click", (e) => {
   //Prevent Default Form Submission Behavior
   e.preventDefault()
+  
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
   //Get Form Values
   let nombres = document.getElementById('nombres').value;
