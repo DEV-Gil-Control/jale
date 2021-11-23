7ce630a89162f72a8863d047eebbb7f436e33ead
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-11-23 16:03:29 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+(function() {
+    $('form > input').keyup(function() {
+
+        var empty = false;
+        $('form > input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#submit').attr('disabled', 'disabled'); 
+        } else {
+            $('#submit').removeAttr('disabled');
+        }
+    });
+})()
+
 //- - - - - - - - -  Push Data - - - - - - - - - - - -//
 
 //Unique Firebase Object
