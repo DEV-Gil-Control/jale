abebc89f64356b37830cf10067eaa8d6bf42fe25
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-11-22 20:08:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -21,9 +21,21 @@
 
 
 //- - - - - - - - -  Push Data - - - - - - - - - - - -//
+
+//Unique Firebase Object
+var firebaseConfig = {
+  apiKey: "AIzaSyDJjGU7r9Dn2UsNTOrCdoHi8MDBNE31-Ic",
+  authDomain: "jale-a6c26.firebaseapp.com",
+  databaseURL: "https://jale-a6c26.firebaseio.com",
+  projectId: "jale-a6c26",
+  storageBucket: "jale-a6c26.appspot.com",
+  messagingSenderId: "125185951298",
+  appId: "1:125185951298:web:3b067709048a1a0edf0fdb"
+};
+
 //Initialize Firebase 
 firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
+var firestore = firebase.firestore()
 
 //Variable to access database collection
 const db = firestore.collection("messages")
