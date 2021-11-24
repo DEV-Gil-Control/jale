6b185496fdda08da3f21e8773bd368e76f568dc0
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-11-24 10:47:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
   var fileButton =         document.getElementById('fileButton');
   fileButton.addEventListener('change', function(e){
   var file = e.target.files[0];
-  var storageRef = firebase.storage().ref('img/'+file.name);
+  var storageRef = firebase.storage().ref('curr/'+file.name);
   var task = storageRef.put(file);
   task.on('state_changed', function progress(snapshot) {
     var percentage = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
